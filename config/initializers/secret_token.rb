628f781b9da5a11e3c3e32c9aa744949fdc798ae
Dakota <kramerdakota@gmail.com>
2012-02-20 11:47:41 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookmarkProject::Application.config.secret_token = '18029c07b1ccee4146d46726c1c5219a628a2ebd811e4c42ad1f52b91612094588405a6c5bad45127520b7626d52c25fc885d3ca46bc621ff64e88a76316c09a'
